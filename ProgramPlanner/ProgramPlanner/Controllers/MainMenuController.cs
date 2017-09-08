6193fb12690198ce04e32963e8127261f31f0a3b
IgnitionOrk <c3179234@uon.edu.au>
2017-09-08 14:35:12 +1000

    Main Menu Stuff

--- ProgramPlanner/ProgramPlanner/Controllers/MainMenuController.cs
+++ ProgramPlanner/ProgramPlanner/Controllers/MainMenuController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace ProgramPlanner.Controllers
 {
@@ -11,22 +12,80 @@ namespace ProgramPlanner.Controllers
     {
         private ProgramPlannerContext db = new ProgramPlannerContext();
 
-        public ActionResult Menu() {
-
-            // If you want to combine YearDegrees and Degrees together use this code
-            /*
-              new SelectList((
-                from d in db.Degrees
-                join yd in db.YearDegrees
-                on d.DegreeID equals yd.DegreeID
-                select new {yd.YearDegreeID, FullName = d.DegreeName + ", " + yd.Year }
-                ), "YearDegreeID", "FullName");
-             */
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index() {
+            // Extract all the Universities in the database.
             ViewBag.Universities = new SelectList(db.Universities, "UniversityID", "UniName");
-            ViewBag.Degrees = new SelectList(db.Degrees, "UniversityID", "DegreeName");
-            ViewBag.Years = new SelectList(Setup.uniqueYearList(db));
-            ViewBag.Majors = new SelectList(db.Majors, "MajorID", "MajorName");    
+
+            // Extract the degrees from the database for the first University. 
+            var degrees = db.Degrees.Where(y => y.UniversityID == db.Universities.FirstOrDefault().UniversityID);
+
+            // Extract the Associated Year Degrees with the first Degree; 
+            var yDegrees = db.YearDegrees.Where(yd => yd.DegreeID == degrees.FirstOrDefault().DegreeID);
+
+            var years = (from y in yDegrees select new { y.YearDegreeID, y.Year});
+
+            // Extract the majors for the first Year Degree.
+            var majors = db.Majors.Where(m => m.YearDegreeID == yDegrees.FirstOrDefault().YearDegreeID);
+
+            ViewBag.Degrees = new SelectList(degrees, "UniversityID", "DegreeName", 0);
+            ViewBag.Years = new SelectList(years,"YearDegreeID", "Year" ,0);
+            ViewBag.Majors = new SelectList(majors, "MajorID", "MajorName", 0);    
             return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="universityID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetDegrees(int universityID)
+        {
+            // Find all the Degrees associated with a University
+            var uDegrees = db.Degrees.Where(degree => degree.UniversityID == universityID);
+            SelectList sl = new SelectList(uDegrees, "DegreeID", "DegreeName");
+            return Json(sl);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="degreeID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetYears(int degreeID)
+        {
+            // Find the YearDegrees associated with the particular Degree (degreeID).
+            var yDegrees = db.YearDegrees.Where(yd => yd.DegreeID == degreeID);
+
+            var years = (from y in yDegrees select new { y.YearDegreeID, y.Year});
+
+            SelectList sl = new SelectList(years, "YearDegreeID", "Year", 0);
+            return Json(sl);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="yearDegreeID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetMajors(int yearDegreeID)
+        {          
+            // Find the YearDegree associated with the particular Degree (degreeID).
+            var yDegree = db.YearDegrees.FirstOrDefault(yd => yd.YearDegreeID == yearDegreeID);
+
+
+            // Find the majors that were held for that particular Year Degree. 
+            var dMajors = db.Majors.Where(major => major.YearDegreeID == yDegree.YearDegreeID);
+
+            SelectList sl = new SelectList(dMajors, "MajorID", "MajorName");
+            return Json(sl);
         }
     }
 }
