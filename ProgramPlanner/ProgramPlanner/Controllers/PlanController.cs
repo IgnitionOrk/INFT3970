845366af5b617d9bb5d8f5a5eb97381bf311123a
Micah Conway <micah.conway1@gmail.com>
2017-09-25 14:27:44 +1000

    Refresh buttons work everywhere

--- ProgramPlanner/ProgramPlanner/Controllers/PlanController.cs
+++ ProgramPlanner/ProgramPlanner/Controllers/PlanController.cs
@@ -271,7 +271,7 @@ namespace ProgramPlanner.Controllers
         }
         [HttpPost]
         public ActionResult isRunningInSemester(int semesterID, string courseCode) {
-            // Need to check if that course was running in the same year as the program planner. 
+            //Need to check if that course was running in the same year as the program planner. 
             int count = 0;
             var abbr = courseCode.Substring(0, 4);
             var abbrID = (db.Abbreviations.Where(
